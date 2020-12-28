82e18593bd86550b99c0f32498f614694cb9dcef
DESKTOP-935SDGT\anh19 <anh195np@gmail.com>
2020-12-28 22:06:05 +0700

    up

--- liet ke cach lay 4 inh vien tu 6 sinh vien/main.cpp
+++ liet ke cach lay 4 inh vien tu 6 sinh vien/main.cpp
@@ -1,44 +1,43 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
 int a[6];
 void view_config(int k){
-     for(int i=1;i<=k;i++)
-    {
-        if(a[i]==1) cout<<"xuan ";
-        else if(a[i]==2) cout<<"ha ";
-        else if(a[i]==3) cout<<"thu ";
-        else if(a[i]==4) cout<<"dong ";
-        else if(a[i]==5) cout<<"nang ";
-        else cout<<"mua";
+    for (int i=1; i<=k; i++) {
+        if (a[i]==1) cout<<"1 ";
+        else if (a[i]==2) cout<<"2 ";
+        else if (a[i]==3) cout<<"3 ";
+        else if (a[i]==4) cout<<"4 ";
+        else if (a[i]==5) cout<<"5 ";
+        else cout<<"6 ";
     }
-    cout<<endl;
+    cout<<"\n";
 }
-void next_config(int k, int i){
+
+void next_config(int k, int i) {
     a[i]+=1;
     i++;
-    while(i<=k){
-        a[i]=a[i-1]+1;
+    while (i<=k) {
+        a[i] = a[i-1] + 1;
         i++;
     }
 }
+
 void config(int n, int k){
     int i;
-    for(i=1; i<=k; i++)
-        a[i]=i;
-    do{
+    for (int i=1; i<=k; i++) a[i] = i;
+    do {
         view_config(k);
-        i=k;
-        while(i>0 && a[i] == n-k+i){
+        i = k;
+        while (i>0 && a[i] == n-k+i) {
             i--;
         }
-        if(i>0) next_config(k, i);
-    }while(i>0);
+        if (i>0) next_config(k, i);
+    } while (i>0);
 }
-int main()
-{
-    int n=5;
+
+int main(){
+    int n = 5;
     config(n, 4);
     return 0;
 }
