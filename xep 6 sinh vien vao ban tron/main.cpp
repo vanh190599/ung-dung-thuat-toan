fe5b508bbe4c788094e36687594cd9232a5e7906
DESKTOP-935SDGT\anh19 <anh195np@gmail.com>
2020-12-28 18:28:25 +0700

    up

---
+++ xep 6 sinh vien vao ban tron/main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+using namespace std;
+int *x,*dd,n;
+
+void xuat()
+{
+    for(int i=1;i<=n;i++){
+        if(x[i]==1) cout<<"tam ";
+        else if(x[i]==2) cout<<"toan ";
+        else if(x[i]==3) cout<<"trang ";
+        else if(x[i]==4) cout<<"cong ";
+        else if(x[i]==5) cout<<"trung ";
+        else  cout<<"tu ";
+    }
+    cout<<endl;
+}
+void Try(int k)
+{
+    for(int i=1;i<=n;i++){
+        if(dd[i]==0){
+            x[k]=i;
+            if(k==n) xuat();
+            else{
+                dd[i]=1;
+                Try(k+1);
+                dd[i]=0;
+            }
+        }
+    }
+}
+
+int main()
+{
+    n=6;
+    x = new int[n+1];
+    dd = new int[n+1];
+    for(int i=1;i<=n;i++)
+    {
+        dd[i]=0;
+    }
+    Try(1);
+    return 0;
+}
